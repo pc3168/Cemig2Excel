e98ca9b9058e7b5d99bf1db0a5077ad1e87d5578
paulo <pc3168@gmail.com>
2026-04-22 22:07:02 -0300

    Criando as classes de modelo.

---
+++ src/main/java/org/example/model/Dados.java
@@ -0,0 +1,30 @@
+package org.example.model;
+
+import java.time.LocalDate;
+
+public class Dados {
+
+    private String numeroNF;
+    private String serie;
+    private String dataEmissao;
+    private String chaveAcesso;
+    private String protocoloAutorizacao;
+    private String unidadeConsumidora;
+    private String codDebitoAutomatico;
+    private String vencimento;
+    private String totalPagar;
+    private String tipoMedicao;
+    private String medicao;
+    private String leituraAnterior;
+    private String leituraAtual;
+    private String consumoKWh;
+    private String icmsBase;
+    private String icmsAliquota;
+    private String icmsValor;
+    private String pasepBase;
+    private String pasepAliquota;
+    private String pasepValor;
+    private String cofinsBase;
+    private String cofinsAliquota;
+    private String cofinsValor;
+}
